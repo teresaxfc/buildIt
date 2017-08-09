f9299b10d55d576248229516c7fbd8ae3dd7b2fa
teresaxfc <teresaxfc@gmail.com>
2017-08-09 14:27:26 +1000

    modify content according to whether user is logged or not

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Header from './Header.jsx';
-import Introduction from './Introduction.jsx';
+import Content from './Content.jsx';
 
 class App extends React.Component {
   render () {
     return (
       <div>
         <Header user={user}/>
-        <Introduction />
+        <Content user={user}/>
       </div>
     );
   }
